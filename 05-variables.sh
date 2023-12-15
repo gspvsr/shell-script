42802a5d669ec7fab7ab7b894a498467174953a8
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 17:38:18 -0600

    again started practicing on another day

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username"
 read USERNAME # this shte variable where the value of th one user entered 
 
 echo "Username entered is : $USERNAME"
+
+ech "Please enter your password"
+
+read PASSWORD
+
+echo "Password entered is : $PASSWORD
