cbd8e7457ac2cdb91c0d70a076f261e9883c4ace
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 17:26:23 -0600

    again started practicing on another day

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#this is the comment, we need to develop a program that can do addition of 2 numbers.
+
+NUMBER1=$1
+NUMBER2=$2
+
+# i need to run a command inside shell to add these 2 numbers
+
+SUM=$(($NUMBER1+$NUMBER2))
