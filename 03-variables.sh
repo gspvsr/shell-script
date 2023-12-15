4ccab055688725be44e984b77ba89b21c2225182
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 17:21:48 -0600

    again started practicing on another day

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo "Hello World, I am learning devops"
-PERSON1=$1 # This is the first value from command line
-PERSON2=$2 # This is the second value from command line
+PERSON1=$1 # this is the first value from command line
+PERSON2=$2 # this is the second value from command line
+ 
+echo "Hello World, I am learning DevOps with AWS"
 
-echo "$PERSON1 : Hi $PERSON2, Good Morning"
-echo "$PERSON2 : Good Morning $PERSON1"
-echo "$PERSON1 : How are you $PERSON2"
-echo "$PERSON2 : I am good, how are you $PERSON1"
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
