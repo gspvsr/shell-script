be94e23c76cdff039813201d8a59b3413a14d3e1
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-14 23:57:37 -0600

    hello world cript add

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Hello World, I am learning devops"
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh : Hi Suresh, Good Morning"
-echo "Suresh : Good Morning Ramesh"
-echo "Ramesh : How are you Suresh"
-echo "Suresh : I am good, how are you ramesh"
+echo "$PERSON1 : Hi Suresh, Good Morning"
+echo "$PERSON2 : Good Morning Ramesh"
+echo "$PERSON1 : How are you Suresh"
+echo "PERSON2 : I am good, how are you ramesh"
