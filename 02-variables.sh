6898af343c8af4b7bfa3c44b3b5fb5913342f240
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 17:18:31 -0600

    again started practicing on another day

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#Shell script run the command inside $() and store the output into varibale called DATE.
 DATE=$(date)
-echo "Hello, this script is executed at timestamp:$DATE"
+
+echo "Hello, this script is executed at timestamp :$DATE"
