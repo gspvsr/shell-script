130ed29e3893feaf2399ef364ce3264fa36c46b4
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 17:53:28 -0600

    again started practicing on another day

--- 06-array.sh
+++ 06-array.sh
@@ -2,5 +2,8 @@
 
 #this is called array, instead of single value it can hold multiple values
 PERSONS=("Ramesh" "Suresh" "Sachin")
+# inside array we always refer values wiht index and it starts from 0
 
 echo "First Person : ${PERSONS[0]}"
+
+echo "All Persons : ${PERSONS[@]}" # disply all names
