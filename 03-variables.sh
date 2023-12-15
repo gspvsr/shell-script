55e1d7a9486976577b68abd79c39ef257f0865f0
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 00:08:05 -0600

    hello world cript add

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello World, I am learning devops"
+PERSON1=$1 # This is the first value from command line
+PERSON2=$2 # This is the second value from command line
+
+echo "$PERSON1 : Hi Suresh, Good Morning"
+echo "$PERSON2 : Good Morning Ramesh"
+echo "$PERSON1 : How are you Suresh"
+echo "$PERSON2 : I am good, how are you ramesh"
