71a1314b22f34c9eccf9e9fd0e33da79c0186bdd
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 00:14:48 -0600

    hello world cript add

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@ echo "Hello World, I am learning devops"
 PERSON1=$1 # This is the first value from command line
 PERSON2=$2 # This is the second value from command line
 
-echo "$PERSON1 : Hi Suresh, Good Morning"
-echo "$PERSON2 : Good Morning Ramesh"
-echo "$PERSON1 : How are you Suresh"
-echo "$PERSON2 : I am good, how are you ramesh"
+echo "$PERSON1 : Hi $PERSON2, Good Morning"
+echo "$PERSON2 : Good Morning $PERSON1"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am good, how are you $PERSON1"
