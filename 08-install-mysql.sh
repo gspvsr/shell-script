aab63c958e52e6915c2f8c98c213b599ea227001
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-16 17:08:23 -0600

    practicing devops

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -21,3 +21,13 @@ then
 else
     echo "Installtion of mysql is success"
 fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "Installtion of postfix is success"
+fi
