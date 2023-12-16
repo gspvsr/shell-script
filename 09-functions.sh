a5a38ee1ad7ebb7b2d0ffb30e73bed0c854d690f
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 19:07:39 -0600

    again started practicing on another day

---
+++ 09-functions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#function :
+#you will keep some code inside functions which is more often repeated, and call whenever you require
+#Functions will do some work on behalf of us and it will update what happened.
+
+USERID=$(id -u)
+# This function should validate the previous command and inform user it is success or failure.
+
+VALIDATE(){
+#$1 ---> it will receive teh argument1
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+    exit 1
+else    
+    echo "Installation of mysql is success"
+fi
+
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR : Please run this script with root access"
+    exit 1
+# else
+#   echo :"INFO:: you are root user"
+fi
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y
+VALIDATE $?
+
+yum install postfix -y
+VALIDATE $?
+
