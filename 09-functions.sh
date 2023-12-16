73f9a1ee1828090ad50e61dbfb993aa2fb064100
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-15 20:42:14 -0600

    again started practicing on another day

--- 09-functions.sh
+++ 09-functions.sh
@@ -8,9 +8,6 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
-USERID=$(id -u)
-
-# This function should validate the previous command and inform user it is success or failure.
 VALIDATE(){
 #?1 will receive teh argument1
 
@@ -23,6 +20,10 @@ else
 fi
 }
 
+USERID=$(id -u)
+# This function should validate the previous command and inform user it is success or failure.
+
+
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR : Please run this script with root access"
@@ -32,9 +33,9 @@ then
 fi
 
 # it is our responsibility again to check installation is success or not
-yum install mysql -y
+yum install mysql -y $>>$LOGFILE
 VALIDATE $? "Installing MYSQL"
 
-yum install postfix -y
+yum install postfix -y $>>$LOGFILE
 VALIDATE $? "Installing postfix"
 
