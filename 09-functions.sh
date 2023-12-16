5023f0f775169a934b509dadce2858ede13446ff
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-16 17:19:23 -0600

    practicing devops

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-DATE=$(date +%F:%H:%M:%S)
-SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-# this function should validate the previous command and inform user it is success or failure
-VALIDATE(){
-    #$1---> it will receive the argument1
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ...... $R FAILURE $N"
-        exit 1
-    else    
-        echo -e "$2 ..... $G success $N"
-    fi
-
-}
+#Our program goal is to be install
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then 
-    echo "ERROR : Please run this script with root access"
+VALIDATE(){
+    #$1---> it will receive the argument1
+
+if [ $1 -ne 0 ]
+then
+    echo "Installation of ..... is FAILURE"
     exit 1
-# else
-#   echo :"INFO:: you are root user"
+else
+    echo "Installtion of ...... is SUCCESS"
 fi
-# it is our responsibility again to check installation is success or not
-yum install mysql -y &>>$LOGFILE
+}
 
-VALIDATE $? "Installing MYSQL"
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : Please run this script with root user"
+    exit 1
+    #not 
+fi
 
-yum install postfix -y &>>$LOGFILE
+# it is our responsibility again to check installin is success or not
+yum install mysql -y 
+VALIDATE $?
 
-VALIDATE $? "Installing Postfix" 
 
-
+yum install postfix -y
+VALIDATE $?
