de74d982357cdc98845eed2aa43e1cdfa19f42fc
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-17 15:58:35 -0600

    my first shell script

--- 00-practice.sh
+++ 00-practice.sh
@@ -2,11 +2,11 @@
 
 USERID$(id -u)
 
-if ($USERID ne 0)
+if [ $USERID -ne 0 ] 
 then
     echo "please proceed with Root User"
 else
-    echo "installation is successful"
+    echo "installation is not successful"
 fi
 
 yum install mysql -y
