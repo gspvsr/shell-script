f653b41d57b4b4ddbcdc0af725b0b4d14e5c6776
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-16 18:13:22 -0600

    practicing devops

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,18 +6,19 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
-R="\e[31m
-G="\e[32m
-N="\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
     #$1---> it will receive the argument1
+
 if [ $1 -ne 0 ]
 then
-    echo "$2.....$R is FAILURE $N"
+    echo -e "$2.....$R is FAILURE $N"
     exit 1
 else
-    echo "$2 ......$G is SUCCESS $N"
+    echo -e "$2 ......$G is SUCCESS $N"
 fi
 }
 
@@ -33,7 +34,6 @@ fi
 # it is our responsibility again to check installin is success or not
 yum install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
-
 
 yum install postfix -y &>>$LOGFILE
 VALIDATE $? "Installing postfix"
