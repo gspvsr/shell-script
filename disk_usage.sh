29fef2beef2b5e344979cf3b0b2bc362cb2b8bd5
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-17 19:35:53 -0600

    my first shell script

--- disk_usage.sh
+++ disk_usage.sh
@@ -22,12 +22,12 @@ DISK_USAGE_THRESHOLD=1
 while IFS= read -r line
 do
     # this command will give you usage in number format for comparison
-    usage=$(echo "$line" | awk '{print $6}' | cut -d % -f1)  # Remove percentage sign
+    usage=$(echo "$line" | awk '{print $6}' | tr -d '%')  # Remove percentage sign
     # this command will give us partition
     partition=$(echo "$line" | awk '{print $1}')
     # now need to check whether it is more than threshold or not
-    if [ "$usage" -gt "$DISK_USAGE_THRESHOLD" ]; then  # Ensure $usage is treated as an integer
-        message+="HIGH DISK USAGE ON $partition: $usage\n"
+    if [ "$usage" -gt "$DISK_USAGE_THRESHOLD" ] 2>/dev/null; then  # Ensure $usage is treated as an integer
+        message+="HIGH DISK USAGE ON $partition: $usage"$'\n'
     fi
 done <<< "$DISK_USAGE"
 
