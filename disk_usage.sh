ec483aad0622d7e34e201600e685eea44f9fca76
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-17 19:09:57 -0600

    my first shell script

--- disk_usage.sh
+++ disk_usage.sh
@@ -21,5 +21,17 @@ DISK_USAGE_THRESHOLD=1
 #IFS=means Internal field seperator is space.
 while IFS= read line
 do
-    echo "output: $line"
+    #this command will give you usage in number format for comparision
+    usage=$(echo $line | awk '{print $6}'| cut -d % -f1)
+    #this command will give us partition
+    partition=$(echo $line | awk '{print $1}')
+    #nwo need to check wheather it is more than threshold or not
+
+    if [#usage -gt $DISK_USAGE_THRESHOLD ];
+    then
+        message+="HIGH DISK USAGE ON $partition: $usage"
+    fi
+}
 done <<< $DISK_USAGE
+
+echo "message: $message"
